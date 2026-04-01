ba93f2433e42caa35c48db18075f4fa42a27505e
harveydevereux <harveydevereux@googlemail.com>
2026-04-01 17:28:03 +0100

    Fix option in discord json

--- src/integrations/discord.rs
+++ src/integrations/discord.rs
@@ -2,19 +2,20 @@ use serde::{Deserialize, Serialize};
 
 use crate::source::Entry;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Image {
     pub url: String
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Embed {
     pub url: String,
     pub title: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub image: Option<Image>
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Post {
     pub content: Option<String>,
     pub embeds: Vec<Embed>
@@ -37,14 +38,15 @@ pub async fn post_discord(webhook: &str, entry: &Entry, name: &str) {
 
 pub async fn post_summary(webhook: &str, entries: Vec<Entry>, header: &str) {
 
+    if entries.len() < 1 { return }
+
     let client = reqwest::Client::new();
 
     let mut embeds = Vec::new();
     for entry in entries {
         embeds.push(entry.as_discord_post(None));
     }
-
-    let post = Post { embeds: embeds, content: Some(header.to_string()) };
+    let mut post = Post { embeds: embeds, content: Some(header.to_string()) };
 
     match client.post(webhook)
         .json(&post)
